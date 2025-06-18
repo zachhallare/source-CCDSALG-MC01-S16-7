1ede54d42dfb48559a2eb87cb0b30ef6ac9a340c
Zach Benedict Hallare <zachhallare@gmail.com>
2025-06-18 16:02:50 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,15 @@
+#include "record.c"
+#include "sortingalgorithms.c"
+#include "filereader.c"
+#include "timer.c"
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    
+
+
+
+    return 0;
+}
---
+++ sortingalgorithms.c
@@ -0,0 +1,66 @@
+#ifndef SORTINGALGORITHMS_C
+#define SORTINGALGORITHMS_C
+
+#include <stdio.h>
+#include "record.c"
+
+/*
+* You may declare additional variables and helper functions
+* as needed by the sorting algorithms here.
+*/
+
+
+
+
+
+
+void insertionSort(Record *arr, int n)
+{
+    // TODO: Implement this sorting algorithm here.
+
+
+
+
+
+
+}
+
+void selectionSort(Record *arr, int n)
+{
+    // TODO: Implement this sorting algorithm here.
+
+
+
+
+
+
+}
+
+void mergeSort(Record *arr, int p, int r)
+{
+    // TODO: Implement this sorting algorithm here.
+
+
+
+
+
+
+}
+
+/*
+* Define AT LEAST ONE more sorting algorithm here, apart from the
+* ones given above. Make sure that the method accepts an array of
+* record structures.
+*/
+
+
+
+
+
+
+
+
+
+
+
+#endif
